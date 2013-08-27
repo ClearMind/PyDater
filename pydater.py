16023a9eea16df88b2799de3d06d71ee657bd50d
Yuzhakov Alexander <clear0mind@gmail.com>
2013-08-27 13:07:17 +0600

    Update pydater.py
    win32

--- pydater.py
+++ pydater.py
@@ -13,22 +13,37 @@ class PyDater(QtGui.QWidget, object):
     def __init__(self):
         super(PyDater, self).__init__()
 
-        self.updates = None # list of urls for downloading
+        self.updates = [] # list of urls for downloading
         self.xml_url = None # full url of xml file with hashes
         self.local_dir_prefix = QtCore.QDir().homePath()
+        self.log_file = None
         self.run = None
 
         # widget layout
         self.layout = QtGui.QVBoxLayout()
         self.pbar = QtGui.QProgressBar()
         self.layout.addWidget(self.pbar)
-        self.log = QtGui.QTextEdit()
-        self.log.setReadOnly(True)
-        self.layout.addWidget(self.log)
+        self.log_ = QtGui.QTextEdit()
+        self.log_.setReadOnly(True)
+        self.layout.addWidget(self.log_)
 
         self.setLayout(self.layout)
         self.setMinimumWidth(500)
         self.setMinimumHeight(300)
+
+    def log(self, strg):
+        self.log_.setText(self.log_.toPlainText() + strg)
+        print strg
+        if not self.log_file:
+            up = os.path.join(str(self.local_dir_prefix), '.updater/')
+            if not os.path.isdir(up):
+                os.makedirs(up)
+            try:
+                self.log_file = open(os.path.join(up, 'updater.log'), 'w')
+            except IOError:
+                print "Can't open file %s" % os.path.join(up, 'updater.log')
+        else:
+            self.log_file.write(strg + "\n")
 
     def setXmlUrl(self, url):
         self.xml_url = url
@@ -40,14 +55,10 @@ class PyDater(QtGui.QWidget, object):
         self.local_dir_prefix = prefix
 
     def check(self):
-        self.log.setText("Checking for updates...\n")
+        self.log("Checking for updates...\n")
         self.updates = []
         try:
             xml_file = urllib2.urlopen(self.xml_url, timeout=5)
-            up = os.path.join(str(self.local_dir_prefix), '.updater/')
-            if not os.path.isdir(up):
-                os.makedirs(up)
-                print 'Directory %s created' % up
 
             xml = xml_file.read()
             root = etree.fromstring(xml)
@@ -59,7 +70,7 @@ class PyDater(QtGui.QWidget, object):
                 if os.path.exists(fn):
                     file = open(fn, 'rb')
                     hash = md5(file.read()).hexdigest()
-                    self.log.setText(self.log.toPlainText()  + "Check " + name_ + '...\n')
+                    self.log("Check " + name_ + '...\n')
                     if hash != f.attrib['hash']:
                         self.updates.append(name_)
                     file.close()
@@ -69,10 +80,8 @@ class PyDater(QtGui.QWidget, object):
 
         except urllib2.HTTPError, e:
             print "Http Error: ", e.code
-            return None
         except urllib2.URLError, e:
             print "URL Error: ", e.reason
-            return None
         except etree.XMLSchemaParseError, e:
             print "XML Parse error %s", e.message
 
@@ -94,12 +103,12 @@ class PyDater(QtGui.QWidget, object):
         i = 0
         for f in self.updates:
             try:
-                self.log.setText(self.log.toPlainText() + f + '\n')
+                self.log(f + '\n')
                 content = urllib2.urlopen(remote_path + f)
-                dir = os.path.dirname(os.path.join(str(self.local_dir_prefix), f[1:] if f[0] == '/' else f))
+                dir = os.path.dirname(os.path.join(str(self.local_dir_prefix), f[1:] if f[0] in '/\\' else f))
                 if not os.path.exists(dir):
                     os.makedirs(dir)
-                file = open(os.path.join(str(self.local_dir_prefix), f[1:] if f[0] == '/' else f), 'wb')
+                file = open(os.path.join(str(self.local_dir_prefix), f[1:] if f[0] in '/\\' else f), 'wb')
                 file.write(content.read())
                 file.close()
                 i += 1
@@ -107,18 +116,26 @@ class PyDater(QtGui.QWidget, object):
 
             except urllib2.HTTPError, e:
                 print e.message
-                return None
+                break
             except IOError, e:
                 print e.message
-                return None
+                break
+
 
     def run_process(self):
-        file = os.path.join(str(self.local_dir_prefix), self.run[1:] if self.run[0] == '/' else self.run)
+        if not self.run:
+            return None
+        file = os.path.join(str(self.local_dir_prefix), self.run[1:] if self.run[0] in '/\\' else self.run)
         os.chmod(file, stat.S_IRWXU | stat.S_IRWXG)
+        ret = 1
         try:
+            self.log('Trying to run process %s' % file)
+            self.log_file.close()
+            self.hide()
             ret = subprocess.check_call(file)
-            QtGui.QApplication.exit(ret)
-            sys.exit(ret)
         except subprocess.CalledProcessError, e:
             print e.message
-        self.close()
+        finally:
+            self.close()
+            QtGui.QApplication.exit(ret)
+            sys.exit(ret)
