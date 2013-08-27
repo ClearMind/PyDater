feb92f746b508f4b848ba69eb1419bfcd952ede1
Yuzhakov Alexander <clear0mind@gmail.com>
2013-08-27 13:08:14 +0600

    Update main.py
    win32

--- main.py
+++ main.py
@@ -3,16 +3,16 @@
 from PyQt4 import QtGui
 import sys
 from pydater import PyDater
+from datetime import datetime
 
 app = QtGui.QApplication(sys.argv)
 
 updater = PyDater()
 updater.show()
-updater.setXmlUrl('http://wp.surgpu.ru/media/program/linux.xml')
-updater.set_local_prefix('/home/student/bin/wp/')
+updater.log(str(datetime.today()))
+updater.setXmlUrl('http://wp.surgpu.ru/media/program/win32.xml')
 updater.check()
 updater.get_updates()
-updater.hide()
 updater.run_process()
 
 app.exec_()
